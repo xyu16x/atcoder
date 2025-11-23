40007b56d3c4bc33cbdb3b62acb7b3cc0f8874e0
xyu16x <xyu16x@gmail.com>
2025-11-24 03:27:33 +0900

    delete

--- abc432/a/main.py
+++
@@ -1,5 +0,0 @@
-n = list(map(int, input().split()))
-
-n.sort(reverse=True)
-
-print("".join(map(str, n)))
--- abc432/e/main.py
+++
@@ -1,3 +0,0 @@
-n, q = map(int, input().split())
-a = list(map(int, input().split()))
-q = [list(input().split()) for _ in range(q)]
